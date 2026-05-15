1318254b7dcdcb21158342fdc8d01c1caf7e5f15
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-05-15 20:00:47 +0300

    C-4 added ViewModel

--- app/src/main/java/dev/toothlonely/contacts/presentation/screen/ContactsScreen.kt
+++ app/src/main/java/dev/toothlonely/contacts/presentation/screen/ContactsScreen.kt
@@ -1,9 +1,13 @@
 package dev.toothlonely.contacts.presentation.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.toothlonely.contacts.data.ContactsProvider
 import dev.toothlonely.contacts.data.ContactsRepositoryImpl
 import dev.toothlonely.contacts.domain.Contact
@@ -11,13 +15,11 @@ import dev.toothlonely.contacts.presentation.screen.component.ContactsList
 
 @Composable
 fun ContactsScreen(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    val resolver = context.contentResolver
-    val provider = ContactsProvider(resolver)
-    val repository = ContactsRepositoryImpl(provider)
-    val contacts = repository.getContacts() ?: emptyMap<Char, List<Contact>>()
 
-    ContactsList(contacts, modifier)
+    val viewModel = hiltViewModel<ContactsViewModel>()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    ContactsList(state.contacts, modifier)
 }
 
 @Composable
---
+++ app/src/main/java/dev/toothlonely/contacts/presentation/screen/ContactsViewModel.kt
@@ -0,0 +1,37 @@
+package dev.toothlonely.contacts.presentation.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.toothlonely.contacts.domain.Contact
+import dev.toothlonely.contacts.domain.ContactsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ContactsViewModel @Inject constructor(
+    private val repository: ContactsRepository
+) : ViewModel() {
+    data class State(
+        val contacts: Map<Char, List<Contact>> = emptyMap()
+    )
+
+    private val _state = MutableStateFlow<State>(State())
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            loadContacts()
+        }
+    }
+
+    private fun loadContacts() {
+        _state.update {
+            it.copy(contacts = repository.getContacts() ?: emptyMap())
+        }
+    }
+}
